1ffad59b60a1f6dc19b6ce13ad819de2688031cc
Simon Phillips <simonxphillips@gmail.com>
2017-11-01 21:12:27 -0400

    ATL

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from darksky import forecast
 from datetime import date, timedelta
 
-BOSTON = 42.3601, 71.0589
+ATLANTA = 33.7537, 84.3863
 
 weekday = date.today()
-with forecast('7a078e9b0afd5187b96a5b60641343bc', *BOSTON) as boston:
-    print(boston.daily.summary)
-    for day in boston.daily:
+with forecast('7a078e9b0afd5187b96a5b60641343bc', *ATLANTA) as atlanta:
+    print(atlanta.daily.summary)
+    for day in atlanta.daily:
         day = dict(day = date.strftime(weekday, '%a'),
                    sum = day.summary,
                    tempMin = day.temperatureMin,
